084bd1e54242303851294c4c92cc358819e2beb6
HuaGuang <2475096613@qq.com>
2024-01-14 12:06:38 +0800

    清除模板，从 0 开始

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+// 完整引入 Element Plus
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(ElementPlus)
+// app.use(router)
 
 app.mount('#app')
